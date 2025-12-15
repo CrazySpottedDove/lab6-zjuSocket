a0f2372d3d6a7dfea97d982743c8ddc30d44d7c6
CrazySpottedDove <3230104178@zju.edu.cn>
2025-12-15 20:40:24 +0800

    rename SocketInfo

--- include/zjuSocket/common.h
+++ include/zjuSocket/common.h
@@ -4,10 +4,10 @@
 namespace zjuSocket {
 using socket_handle_t = int;
 using port_t          = unsigned short;
-struct ClientInfo
+struct SocketInfo
 {
-    socket_handle_t socket_handle;
-    std::string     client_ip_address;
-    port_t          client_port;
+    socket_handle_t handle;
+    std::string     ip_address;
+    port_t          port;
 };
 }   // namespace zjuSocket
--- server/server.h
+++ server/server.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "zjuSocket/common.h"
 #include "zjuSocket/genericServer.h"
+#include <mutex>
+#include <vector>
 
 namespace zjuSocket {
 class Server : public GenericServer<Server>
@@ -9,6 +12,7 @@ public:
     Server();
     void Run();
 private:
-    
+    std::vector<SocketInfo> clients_;
+    std::mutex clientsMutex_;
 };
 }   // namespace zjuSocket
